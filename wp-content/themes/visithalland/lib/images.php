6a558bd0974246594c84dab03997bb32d7442079
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2017-02-08 14:13:41 +0100

    Add hero image size

--- wp-content/themes/visithalland/lib/images.php
+++ wp-content/themes/visithalland/lib/images.php
@@ -1,4 +1,6 @@
 <?php
+
+//Modify inserted images to be enclosed by a figure tag
 function title_caption_image($html, $id, $caption, $title, $align, $url, $size, $alt) {
     $src  = wp_get_attachment_image_src( $id, $size, false );
     $html5 = "<figure class='align$align'>";
@@ -10,4 +12,8 @@ function title_caption_image($html, $id, $caption, $title, $align, $url, $size, 
     return $html5;
 }
 add_filter( 'image_send_to_editor', 'title_caption_image', 10, 9 );
+
+
+//Add image size for hero images
+add_image_size( 'hero', 1920, 550 ); // 1920 pixels wide by 550 pixels tall, soft proportional crop mode
 ?>
