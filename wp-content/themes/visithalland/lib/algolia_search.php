8ef638b1c5ba148e4b6d35c92440ba0b3979ce02
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2017-05-29 17:09:47 +0200

    Add support for custom fields on every post type in algolia

--- wp-content/themes/visithalland/lib/algolia_search.php
+++ wp-content/themes/visithalland/lib/algolia_search.php
@@ -15,7 +15,7 @@ add_filter( 'algolia_searchable_post_shared_attributes', 'my_post_attributes', 1
  */
 function my_post_attributes( array $attributes, WP_Post $post ) {
 
-    if ( 'places' !== $post->post_type ) {
+    if ( 'places' !== $post->post_type || 'happening' !== $post->post_type || 'trip' !== $post->post_type || 'editor_tip' !== $post->post_type || 'meet_local' !== $post->post_type || 'companies' !== $post->post_type ) {
         // We only want to add an attribute for the 'speaker' post type.
         // Here the post isn't a 'speaker', so we return the attributes unaltered.
         return $attributes;
@@ -25,8 +25,13 @@ function my_post_attributes( array $attributes, WP_Post $post ) {
     // @see https://www.advancedcustomfields.com/resources/get_field/
     $attributes['cover_image'] = get_field('cover_image', $post->ID)["sizes"]["medium_large"];
     $attributes['description'] = get_field('description', $post->ID);
-    $attributes['location'] = get_field('location', $post->ID);
     $attributes['post_name'] = $post->post_name;
+
+    //Place specific
+    $location = get_field('location', $post->ID)
+    if($location) {
+        $attributes['location'] = get_field('location', $post->ID);
+    }
 
     // Always return the value we are filtering.
     return $attributes;
