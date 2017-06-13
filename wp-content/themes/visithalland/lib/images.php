3c07d21b0598318dc96dad92c23ec0bffcf38e28
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2017-06-13 11:44:58 +0200

    Add new image sizes

--- wp-content/themes/visithalland/lib/images.php
+++ wp-content/themes/visithalland/lib/images.php
@@ -14,6 +14,62 @@ function title_caption_image($html, $id, $caption, $title, $align, $url, $size, 
 add_filter( 'image_send_to_editor', 'title_caption_image', 10, 9 );
 
 
+/* 
+	Add custom image sizes
+*/
 //Add image size for hero images
+add_image_size( 'vh_profile', 75, 75, array( 'center', 'top' ) );
+add_image_size( 'vh_profile@2x', 150, 150, array( 'center', 'top' ) );
+
+add_image_size( 'vh_thumbnail', 140, 105, array( 'center', 'center' ) );
+add_image_size( 'vh_thumbnail@2x', 280, 210, array( 'center', 'center' ) );
+
+add_image_size( 'vh_small', 400, 225, array( 'center', 'center' ) );
+add_image_size( 'vh_small@2x', 800, 450, array( 'center', 'center' ) );
+
+add_image_size( 'vh_medium', 560, 315, array( 'center', 'center' ) );
+add_image_size( 'vh_medium@2x', 1120, 630, array( 'center', 'center' ) );
+
+add_image_size( 'vh_large', 960, 720, array( 'center', 'center' ) );
+add_image_size( 'vh_large@2x', 1920, 1440, array( 'center', 'center' ) );
+
+add_image_size( 'vh_hero_wide', 1440, 810, array( 'center', 'center' ) );
+add_image_size( 'vh_hero_wide@2x', 2880, 1620, array( 'center', 'center' ) );
+
+add_image_size( 'vh_hero_tall', 400, 500, array( 'center', 'center' ) );
+add_image_size( 'vh_hero_tall@2x', 800, 1000, array( 'center', 'center' ) );
+
 add_image_size( 'hero', 1920, 1200 ); // 1920 pixels wide by 550 pixels tall, soft proportional crop mode
+
+/* Remove default image sizes to save space and cpu load on upload */
+/*function remove_default_image_sizes( $sizes) {
+    unset($sizes['thumbnail']);
+    unset($sizes['medium']);
+    unset($sizes['medium_large']);
+    unset($sizes['large']);
+    return $sizes;
+}
+add_filter('intermediate_image_sizes_advanced','remove_default_image_sizes');*/
+
+
+//Add support for choosing our image sizes in Wysiyg admin
+add_filter( 'image_size_names_choose', 'my_custom_sizes' ); 
+function my_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'vh_profile' => __( 'Profile' ),
+        'vh_profile@2x' => __( 'Profile @2x' ),
+        'vh_thumbnail' => __( 'Thumbnail' ),
+        'vh_thumbnail@2x' => __( 'Thumbnail @2x' ),
+        'vh_small' => __( 'Small' ),
+        'vh_small@2x' => __( 'Small @2x' ),
+        'vh_medium' => __( 'Medium' ),
+        'vh_medium@2x' => __( 'Medium @2x' ),
+        'vh_large' => __( 'Large' ),
+        'vh_large@2x' => __( 'Large @2x' ),
+        'vh_hero_wide' => __( 'Hero Wide' ),
+        'vh_hero_wide@2x' => __( 'Hero Wide @2x' ),
+        'vh_hero_tall' => __( 'Hero Tall' ),
+        'vh_hero_tall@2x' => __( 'Hero Tall @2x' ),
+    ) );
+}
 ?>
