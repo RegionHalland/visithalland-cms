9eff0402371e1aaeeee557c3a960ec1ef6e1d116
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2017-12-20 09:44:45 +0100

    Add more WIP to concept (page.php)

--- wp-content/themes/visithalland/page.php
+++ wp-content/themes/visithalland/page.php
@@ -140,45 +140,178 @@
 	<?php //wp_die(print_r(vh_get_meet_local_by_taxonomy_concept()[0]->post_title)); ?>
 
 	<?php /* START - ArticleFull */ ?>
-	<article class="article-full relative my5">
-		<div class="article-full__img-container topographic-pattern">
-		    <picture>
-				<source media="(min-width: 768px)"
-					srcSet="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_large"] . " 1x," . get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_large@2x"] . " 2x" ?>" />
-				<source
-					srcSet="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_medium"] . " 1x," . get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_medium@2x"] . " 2x" ?>" />
-				<img class="concept-header__img" src="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_hero_wide"] ?>" alt="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["alt"] ?>" />
-		    </picture>
-		</div>
-		<div class="article-full__scrim absolute left-0 right-0 bottom-0 z1"></div>
-		<div class="article-full__inner absolute bottom-0 left-0 right-0 z2 clearfix">
-		<div class="article-full__content col col-12 md-col-6 lg-col-6 <?php echo vh_get_pretty_post_type_name(get_field("featured")[2]->post_type) ?>">
-
-			<div class="article-tag mt3 mb2">
-				<div class="article-tag__icon-wrapper">
-					<div class="article-tag__icon"></div>
-				</div>
-				<span class="article-tag__type">
-				<?php echo vh_get_pretty_post_type_name(vh_get_meet_local_by_taxonomy_concept()[0]->post_type) ?>
-				</span>
+	<?php if(isset(vh_get_meet_local_by_taxonomy_concept()[0])) : ?>
+		<article class="article-full relative my5">
+			<div class="article-full__img-container topographic-pattern">
+			    <picture>
+					<source media="(min-width: 768px)"
+						srcSet="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_large"] . " 1x," . get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_large@2x"] . " 2x" ?>" />
+					<source
+						srcSet="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_medium"] . " 1x," . get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_medium@2x"] . " 2x" ?>" />
+					<img class="concept-header__img" src="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["sizes"]["vh_hero_wide"] ?>" alt="<?php echo get_field("cover_image", vh_get_meet_local_by_taxonomy_concept()[0]->ID)["alt"] ?>" />
+			    </picture>
 			</div>
-		    
-		    <a href="<?php echo vh_get_meet_local_by_taxonomy_concept()[0]->guid ?>" class="link-reset article-full__link link light">
-		        <h2 class="article-full__title light mt1 mb2"><?php echo vh_get_meet_local_by_taxonomy_concept()[0]->post_title ?></h2>
-		        <p class="article-full__excerpt mb2"><?php echo get_field("excerpt", vh_get_meet_local_by_taxonomy_concept()[0]->ID) ?></p>
-		        <div class="article-link inline-block mt0">
-		            <hr class="article-link__divider block mb3"/>
-		            <span class="article-link__text">Läs hela artikeln</span>
-		            <span class="article-link__icon-wrapper">
-		                <i class="material-icons article-link__icon">arrow_forward</i>
-		            </span>
-		        </div>
-		    </a>
-		</div>    
-		</div>
-	</article>
-		
+			<div class="article-full__scrim absolute left-0 right-0 bottom-0 z1"></div>
+			<div class="article-full__inner absolute bottom-0 left-0 right-0 z2 clearfix">
+				<div class="article-full__content col col-12 md-col-6 lg-col-6 <?php echo vh_get_pretty_post_type_name(get_field("featured")[2]->post_type) ?>">
+
+					<div class="article-tag mt3 mb2">
+						<div class="article-tag__icon-wrapper">
+							<div class="article-tag__icon"></div>
+						</div>
+						<span class="article-tag__type">
+						<?php echo vh_get_pretty_post_type_name(vh_get_meet_local_by_taxonomy_concept()[0]->post_type) ?>
+						</span>
+					</div>
+				    
+				    <a href="<?php echo vh_get_meet_local_by_taxonomy_concept()[0]->guid ?>" class="link-reset article-full__link link light">
+				        <h2 class="article-full__title light mt1 mb2"><?php echo vh_get_meet_local_by_taxonomy_concept()[0]->post_title ?></h2>
+				        <p class="article-full__excerpt mb2"><?php echo get_field("excerpt", vh_get_meet_local_by_taxonomy_concept()[0]->ID) ?></p>
+				        <div class="article-link inline-block mt0">
+				            <hr class="article-link__divider block mb3"/>
+				            <span class="article-link__text">Läs hela artikeln</span>
+				            <span class="article-link__icon-wrapper">
+				                <i class="material-icons article-link__icon">arrow_forward</i>
+				            </span>
+				        </div>
+				    </a>
+				</div>    
+			</div>
+		</article>
+	<?php endif; ?>
+	<?php /* END - ArticleFull */ ?>
+
+
 	<?php /* START - ArticleFull */ ?>
+		<div class="clearfix concept-content mt2">
+		    <div class="concept-grid col col-12 md-col-12 lg-col-8 no-gutter">
+		        <div class="clearfix mxn2">
+		        	<?php 
+		        		$posts = vh_get_posts_without_happenings_by_taxonomy_concept($post->ID, -1);
+		        		foreach ($posts as $index => $value) : ?>
+		        			<?php if(($index + 1) % 3 === 0) : ?>
+		        				<div class="concept-grid__item col col-12">
+								<article class="article-image relative <?php echo vh_get_post_taxonomy()["slug"] ?>">
+									<div class="article-image__img-container topographic-pattern">
+										<picture>
+											<source media="(min-width: 700px)"
+												srcSet="<?php echo get_field("cover_image", $value->ID)["sizes"]["vh_large"] . " 1x," . get_field("cover_image", $value->ID)["sizes"]["vh_large@2x"] . " 2x" ?>" />
+											<source
+												srcSet="<?php echo get_field("cover_image", $value->ID)["sizes"]["vh_hero_tall"] . " 1x," . get_field("cover_image", $value->ID)["sizes"]["vh_hero_tall@2x"] . " 2x" ?>" />
+											<img class="concept-header__img" src="<?php echo get_field("cover_image", $value->ID)["sizes"]["vh_large"] ?>" alt="<?php echo get_field("cover_image", $value->ID)["alt"] ?>" />
+										</picture>
+									</div>
+									<a class="link-reset">
+										<div class="article-image__content absolute left-0 bottom-0">
+											<div class="article-tag--light mt3 mb2">
+												<div class="article-tag__icon-wrapper">
+													<div class="article-tag__icon"></div>
+												</div>
+												<span class="article-tag__type">
+													<?php echo vh_get_pretty_post_type_name(get_field("featured")[0]->post_type) ?>
+												</span>
+											</div>
+											<h2 class="article-image__title light my1"><?php echo $value->post_title ?></h2>
+											<div class="article-link inline-block mt0">
+												<hr class="article-link__divider block mb3"/>
+												<span class="article-link__text">Läs hela artikeln</span>
+												<span class="article-link__icon-wrapper">
+												<i class="material-icons article-link__icon">arrow_forward</i>
+												</span>
+											</div>
+										</div>
+									</a>
+								</article>
+		        				</div>
+		        			<?php else: ?>
+		        				<div style="outline:blue solid thick;" class="concept-grid__item col col-12 sm-col-6">
+		        					<h1><?php echo $value->post_title ?></h1>
+		        				</div>
+		        			<?php endif ?>
+		       			<?php endforeach ?>
+
+
+		            this.props.posts.map(function(el, index) {
+		                //Every third article should be ArticleLargeGridItem
+		                if ((index + 1) % 3 === 0) {
+		                    return <div key={el.ID.toString()} class="concept-grid__item col col-12">
+		                                <ArticleImage
+		                                    post_title={_.get(el, 'post_title', '')}
+		                                    post_type={_.get(el, 'post_type', '')}
+		                                    post_name={_.get(el, 'post_name', '')}
+		                                    post_concept={_.get(el, 'taxonomy.name', '')}
+		                                    excerpt={_.get(el, 'meta_fields.excerpt', '')}
+		                                    taxonomy_slug={_.get(el, 'taxonomy.slug', '')}
+		                                    cover_image_large_2x={_.get(el, 'meta_fields.cover_image.sizes.vh_large@2x', '')}
+		                                    cover_image_large={_.get(el, 'meta_fields.cover_image.sizes.vh_large', '')}
+		                                    cover_image_tall_2x={_.get(el, 'meta_fields.cover_image.sizes.vh_hero_tall@2x', '')}
+		                                    cover_image_tall={_.get(el, 'meta_fields.cover_image.sizes.vh_hero_tall', '')}
+		                                    cover_image_alt={_.get(el, 'meta_fields.cover_image.alt', '')}
+		                                />
+		                            </div>
+		                }
+		                return <div key={el.ID.toString()} class="concept-grid__item col col-12 sm-col-6">
+		                            <ArticleMedium
+		                                    post_title={_.get(el, 'post_title', '')}
+		                                    post_type={_.get(el, 'post_type', '')}
+		                                    post_name={_.get(el, 'post_name', '')}
+		                                    excerpt={_.get(el, 'meta_fields.excerpt', '')}
+		                                    taxonomy_slug={_.get(el, 'taxonomy.slug', '')}
+		                                    cover_image_small={_.get(el, 'meta_fields.cover_image.sizes.vh_small', '')}
+		                                    cover_image_small_2x={_.get(el, 'meta_fields.cover_image.sizes.vh_small@2x', '')}
+		                                    cover_image_medium={_.get(el, 'meta_fields.cover_image.sizes.vh_medium', '')}
+		                                    cover_image_medium_2x={_.get(el, 'meta_fields.cover_image.sizes.vh_medium@2x', '')}
+		                                    cover_image_alt={_.get(el, 'meta_fields.cover_image.alt', '')}
+		                            />
+		                        </div>
+		            })
+		        }
+		        
+		        </div>
+		    </div>
+		    <div class="concept-sidebar col col-12 lg-col-4">
+		        <div class="concept-happenings clearfix">
+		            {   
+		                this.props.happenings.map(function(el, index) {
+		                    return <div key={el.ID.toString()} class="concept-happenings__item col col-12 sm-col-6 lg-col-12">
+		                                <HappeningListItem 
+		                                    post_title={_.get(el, 'post_title')} 
+		                                    post_name={_.get(el, 'post_name')}
+		                                    post_type={_.get(el, 'post_type')}
+		                                    excerpt={_.get(el, 'meta_fields.excerpt')}
+		                                    taxonomy_slug={_.get(el, 'taxonomy.slug', '')}
+		                                    start_date={_.get(el, 'meta_fields.start_date')}
+		                                    cover_image_thumbnail={_.get(el, 'meta_fields.cover_image.sizes.vh_thumbnail')}
+		                                    cover_image_alt={_.get(el, 'meta_fields.cover_image.alt')} 
+		                                />
+		                            </div>
+		                })
+		            }
+		        </div>
+		        <div class="concept-thumbnails clearfix">
+		            {   
+		                this.props.menuItems.map(function(el, index) {
+		                    return <div key={el.ID.toString()} class="concept-thumbnails__item col col-12 sm-col-6 lg-col-12">
+		                                <ConceptThumbnailSmall
+		                                    post_name={_.get(el, 'post_name', '')}
+		                                    post_title={_.get(el, 'post_title', '')}
+		                                    excerpt={_.get(el, 'excerpt', '')} 
+		                                    cover_image_tall={_.get(el, 'cover_image.sizes.vh_hero_tall', '')}
+		                                    cover_image_tall_2x={_.get(el, 'cover_image.sizes.vh_hero_tall@2x', '')}  
+		                                    cover_image_large={_.get(el, 'cover_image.sizes.vh_large', '')}
+		                                    cover_image_large_2x={_.get(el, 'cover_image.sizes.vh_large@2x', '')}
+		                                    featured={_.get(el, 'featured', [])}
+		                                />
+		                            </div>
+		                })
+		            }
+		        </div>
+		    </div>
+		</div>
+	<?php /* END - ArticleFull */ ?>
+
+		
+	
 </main>
 
 <?php get_footer(); ?>
