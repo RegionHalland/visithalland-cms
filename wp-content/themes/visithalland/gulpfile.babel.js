f6ad7b2f97d86913f22254e9dbf830431e354308
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-01-03 09:06:25 +0100

    Add wip gulp env

---
+++ wp-content/themes/visithalland/gulpfile.babel.js
@@ -0,0 +1,74 @@
+'use strict';
+
+import gulp from 'gulp'
+import shell from 'gulp-shell'
+import plumber from 'gulp-plumber'
+import gutil from 'gulp-util'
+import browsersync from 'browser-sync'
+import rename from 'gulp-rename'
+import sass from 'gulp-sass'
+import postcss from 'gulp-postcss'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+import concat from 'gulp-concat'
+import uglify from 'gulp-uglify'
+
+// Build CSS
+gulp.task('css:dist', () => {
+	// PostCSS plugins
+	const plugins = [
+		autoprefixer({browsers: ['last 1 version']}),
+		cssnano({ mergeLonghand: false, zindex: false })
+	];
+	return gulp.src('./assets/src/scss/*.scss')
+		.pipe(plumber({
+			errorHandler: error => {
+				gutil.beep()
+				console.log(error)
+			}
+		}))
+		.pipe(sass())
+		.pipe(postcss(plugins))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(plumber.stop())
+		.pipe(gulp.dest('./assets/dist/css/'))
+		.pipe(browsersync.stream())
+})
+
+// Build JS
+gulp.task('js:dist', () => {
+	// App
+	gulp.src('./assets/src/js/*.js')
+		.pipe(plumber({
+			errorHandler: error => {
+				gutil.beep()
+				console.log(error)
+			}
+		}))
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./assets/dist/js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./assets/dist/js/'))
+})
+
+// Browsersync
+gulp.task('browsersync', () => {
+	browsersync.init({
+		proxy: 'cms.visithalland.test'
+	});
+})
+
+// Browsersync reload
+gulp.task('bs-reload', () => {
+	browsersync.reload();
+})
+
+// Watch
+gulp.task('watch', ['js:dist', 'css:dist', 'browsersync'], () => {
+	gulp.watch('./assets/src/scss/**/*.scss', ['css:dist', 'bs-reload']);
+	gulp.watch(['./assets/src/js/**/*.js'], ['js:dist', 'bs-reload']);
+})
+
+// Default build
+gulp.task('default', ['css:dist', 'js:dist'])
