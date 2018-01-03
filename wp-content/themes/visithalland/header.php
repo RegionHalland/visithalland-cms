fa4d78e011e639af4fab46ddd7df7f7582bd7a8f
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-01-03 13:58:54 +0100

    Add Flickity

--- wp-content/themes/visithalland/header.php
+++ wp-content/themes/visithalland/header.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title><?php echo wp_get_document_title(); ?></title>
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/css/main.min.css" />
     <link rel="stylesheet" href="https://use.typekit.net/vzi2bvt.css" />
-            
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 </head>
 <body>
 
