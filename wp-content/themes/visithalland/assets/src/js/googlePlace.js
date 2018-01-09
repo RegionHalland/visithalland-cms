1ea7805514a36240d93ab581cc4d1fe6ddffd2d5
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-01-09 10:40:00 +0100

    Add WIP Google Place calls

--- wp-content/themes/visithalland/assets/src/js/googlePlace.js
+++ wp-content/themes/visithalland/assets/src/js/googlePlace.js
@@ -1,3 +1,45 @@
 jQuery(function() {
 	console.log("place", php_vars);
+
+      var map;
+      var service;
+      initMap();
+
+      function initMap() {
+        var locationToSearch = {lat: 56.36948109999999, lng: 13.16865919999998};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: locationToSearch,
+          zoom: 15
+        });
+
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: locationToSearch,
+          radius: 500,
+          keyword: "Kungsbygget Äventyrspark"
+        }, callback);
+      }
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          console.log(results);
+          var place = results[0];
+          service.getDetails({placeId: place.place_id}, detailCallback);
+          /*for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }*/
+        }
+      }
+
+          function detailCallback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+              console.log(results)
+                /*vm.phone = results.international_phone_number ? results.international_phone_number : results.formatted_phone_number;
+                vm.website = results.website;
+                vm.openNow = results.opening_hours ? results.opening_hours.open_now : false;
+                vm.openingHours = results.opening_hours ? results.opening_hours.weekday_text : [];
+                vm.forceUpdate();*/
+            }
+        }
 });
