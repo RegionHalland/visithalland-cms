37ef89d15cc582685a63da0bebbd67cb3f386401
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-01-24 15:30:55 +0100

    Add next links to single pages

--- wp-content/themes/visithalland/single-trip.php
+++ wp-content/themes/visithalland/single-trip.php
@@ -160,12 +160,8 @@
 	</article>
 <?php endwhile; ?>
 
-<div class="previous-post-link">
-	<?php previous_post_link('%link', '<< Previous Post', $in_same_term = true, $excluded_terms = '', $taxonomy = 'taxonomy_concept'); ?>
-</div>
-
 <div class="next-post-link">
-	<?php next_post_link('%link', 'Next Post >>', $in_same_term = true, $excluded_terms = '', $taxonomy = 'taxonomy_concept'); ?>                    
+    <?php next_post_link('%link', 'Next Post >>', $in_same_term = true, $excluded_terms = '', $taxonomy = 'taxonomy_concept'); ?>                    
 </div>
 
 </div>
