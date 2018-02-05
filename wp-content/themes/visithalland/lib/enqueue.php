75f433d9c60c0c1b7dc295b0762376b7d753fa9e
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-02-05 15:53:59 +0100

    Add scripts to footer

--- wp-content/themes/visithalland/lib/enqueue.php
+++ wp-content/themes/visithalland/lib/enqueue.php
@@ -7,11 +7,8 @@ function visithalland_scripts() {
 	global $post;
 	$postData = array();
 
-	wp_enqueue_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCwAJMUt4ODfivqIGMgeZeTlH90-QqRFrU&libraries=places', array( 'jquery' ) );
 	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/assets/dist/js/app.min.js', array( 'jquery' ) );
-	wp_enqueue_script( 'infinite-scroll', 'https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js', array( 'jquery' ) );
 	wp_enqueue_style( 'stylesheet', get_stylesheet_directory_uri() . '/assets/dist/css/main.min.css' );
-	//wp_enqueue_style ('typekit', "https://use.typekit.net/vzi2bvt.css");
 	
 	if($post){
 		$postData = array(
