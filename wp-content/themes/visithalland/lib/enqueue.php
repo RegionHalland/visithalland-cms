0b4aed42b38668a9a74f0f03da5459e1e55a7f6d
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-02-05 16:25:01 +0100

    #44 Add footer variable to enqueue

--- wp-content/themes/visithalland/lib/enqueue.php
+++ wp-content/themes/visithalland/lib/enqueue.php
@@ -9,10 +9,18 @@ function visithalland_scripts() {
 
 
 	wp_deregister_script('jquery');
-    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js#asyncload', array(), null, true);
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
 
-	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/assets/dist/js/app.min.js#asyncload', array( 'jquery' ) );
+	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/assets/dist/js/app.min.js', array( 'jquery' ), null, true );
 	wp_enqueue_style( 'stylesheet', get_stylesheet_directory_uri() . '/assets/dist/css/main.min.css' );
+
+	wp_enqueue_script( 'flickity-script', 'https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', array( 'jquery' ), null, true );
+	
+	wp_enqueue_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCwAJMUt4ODfivqIGMgeZeTlH90-QqRFrU&libraries=places', array( 'jquery' ), null, true );
+	
+	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/assets/dist/js/app.min.js', array( 'jquery' ), null, true );
+	
+	wp_enqueue_script( 'infinite-scroll', 'https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js', array( 'jquery' ), null, true );
 	
 	if($post){
 		$postData = array(
