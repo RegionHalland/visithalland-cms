444fce190614339d31445310fe9453bdee753bfe
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-02-06 09:56:55 +0100

    #44 Move scripts to footer, remove unused pngs and replaced with data-uris.

--- wp-content/themes/visithalland/functions.php
+++ wp-content/themes/visithalland/functions.php
@@ -140,6 +140,14 @@ include_once('lib/rest/v2/callbacks.php');
 //include_once('lib/helpers.php');
 
 
+
+//Remove unwanted head tags from project
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+
+
 // Async load
 function rh_async_scripts($url)
 {
--- wp-content/themes/visithalland/lib/enqueue.php
+++ wp-content/themes/visithalland/lib/enqueue.php
@@ -1,14 +1,24 @@
 <?php
 
 
-add_filter( 'wp_enqueue_scripts', 'visithalland_scripts', 0 );
-
 function visithalland_scripts() {
 	global $post;
 	$postData = array();
 
-	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/assets/dist/js/app.min.js', array( 'jquery' ) );
-	wp_enqueue_style( 'stylesheet', get_stylesheet_directory_uri() . '/assets/dist/css/main.min.css' );
+
+	//Add custom jQuery to avoid scripts in <head>
+	if (!is_admin()) {
+		wp_deregister_script('jquery');
+	    wp_register_script('vh-jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', NULL, false, true);
+	    wp_enqueue_script('vh-jquery');
+    }
+
+    wp_enqueue_style( 'stylesheet', get_stylesheet_directory_uri() . '/assets/dist/css/main.min.css' );
+
+	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/assets/dist/js/app.min.js', array( 'vh-jquery' ), false, true );
+	wp_enqueue_script( 'flickity', 'https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', array( 'vh-jquery' ), false, true );
+	wp_enqueue_script( 'infinite-scroll', 'https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js', array( 'vh-jquery' ), false, true );
+	wp_enqueue_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCwAJMUt4ODfivqIGMgeZeTlH90-QqRFrU&libraries=places', array( 'vh-jquery' ), false, true );
 	
 	if($post){
 		$postData = array(
@@ -19,6 +29,6 @@ function visithalland_scripts() {
 
 	wp_localize_script( 'app', 'phpVars', $postData);
 }
-add_action( 'wp_enqueue_scripts', 'visithalland_scripts' );
 
+add_action( 'wp_enqueue_scripts', 'visithalland_scripts', 20 );
 
