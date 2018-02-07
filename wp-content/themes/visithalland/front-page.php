c40696b171cb6d9f06d658d56350d8037a03b6e3
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-02-07 13:32:52 +0100

    #44 Add feature image to frontpage

--- wp-content/themes/visithalland/front-page.php
+++ wp-content/themes/visithalland/front-page.php
@@ -5,12 +5,12 @@
     		<span class="landing-header__logo center">
     			<img data-src="<?php echo get_stylesheet_directory_uri(); ?>/assets/src/img/landing-logo.svg">
     		</span>
-			<picture>
-				<source media="(min-width: 60em)"
-					data-srcset="<?php echo get_field("cover_image", get_option( 'page_on_front' ))["sizes"]["vh_hero_wide"] . " 1x," . get_field("cover_image", get_option( 'page_on_front' ))["sizes"]["vh_hero_wide@2x"] . " 2x" ?>" />
-				<source
-					data-srcset="<?php echo get_field("cover_image", get_option( 'page_on_front' ))["sizes"]["vh_hero_tall"] . " 1x," . get_field("cover_image", get_option( 'page_on_front' ))["sizes"]["vh_hero_tall@2x"] . " 2x" ?>" />
-				<img class="concept-header__img" data-src="<?php echo get_field("cover_image", get_option( 'page_on_front' ))["sizes"]["vh_hero_wide"] ?>" alt="<?php echo get_field("cover_image", get_option( 'page_on_front' ))["alt"] ?>" />
+    		<picture>
+				<source media="(min-width: 60em)" data-srcset="<?php echo get_the_post_thumbnail_url(get_option( 'page_on_front' ), 'vh_hero_wide' ) . " 1x," . get_the_post_thumbnail_url(get_option( 'page_on_front' ), 'vh_hero_wide@2x' ) . " 2x" ?>" />
+				<source data-srcset="<?php echo get_the_post_thumbnail_url(get_option( 'page_on_front' ), 'vh_hero_tall' ) . " 1x," . get_the_post_thumbnail_url(get_option( 'page_on_front' ), 'vh_hero_tall@2x' ) . " 2x" ?>" />
+				<img class="concept-header__img" 
+					data-src="<?php echo get_the_post_thumbnail_url(get_option( 'page_on_front' ), 'vh_hero_wide' )?>"
+					alt="<?php echo get_field("cover_image", get_option( 'page_on_front' ))["alt"] ?>" />
 			</picture>
 		</div>
 	</div>
