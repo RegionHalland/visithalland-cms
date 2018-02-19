69eb31529a99f4b232fcd2f1f22818431d6a21aa
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-02-19 16:42:48 +0100

    Separate JS-files and fixed fadein for lazy load

---
+++ wp-content/themes/visithalland/assets/src/js/cookie.js
@@ -0,0 +1,18 @@
+jQuery(function() {
+	// Init Cookie-banner Star
+    $('#cookie-accept').on('click', function () { 
+	  days = 182; //number of days to keep the cookie
+	  myDate = new Date();
+	  myDate.setTime(myDate.getTime()+(days*24*60*60*1000));
+	  document.cookie = "comply_cookie = comply_yes; expires = " + myDate.toGMTString(); //creates the cookie: name|value|expiry
+	  $(".cookie-banner").fadeOut(300); //$ to slide it up
+	});
+
+	$('#eu-btn').on('click', function () { 
+	  days = 10; //number of days to keep the cookie
+	  myDate = new Date();
+	  myDate.setTime(myDate.getTime()+(days*24*60*60*1000));
+	  document.cookie = "hide_eu = comply_yes; expires = " + myDate.toGMTString(); //creates the cookie: name|value|expiry
+	  $(".landing-eu").fadeOut(300); //$ to slide it up
+	});
+});
