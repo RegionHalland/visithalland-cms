57f34292a14f10a5cadc58c37d4c616ff6a46799
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-02-26 16:32:45 +0100

    #122 Add users to about page

--- wp-content/themes/visithalland/page.php
+++ wp-content/themes/visithalland/page.php
@@ -5,8 +5,6 @@
 	$author_id = get_the_author_meta('ID');
 
 ?>
-
-
 	<article class="container <?php echo vh_get_post_taxonomy()['slug']; ?>" role="main" id="main-content">
 		<div class="page-header">
 			<div class="page-header__backdrop topographic-pattern">
@@ -39,6 +37,36 @@
 	    		<div class="article-body col col-12 sm-col-9 md-col-9 page__body">
 			    	<?php the_content(); ?>
 			    </div>
+			    <div class="col col-12 sm-col-9 md-col-9 page__body">
+			    	<?php if( have_rows('contact') ): ?>
+	                    <div class="contacts mt2 clearfix">
+	                        <div class="contacts__header">
+	                            <h3><?php _e( 'Kontaktpersoner', 'visithalland' ); ?></h3>
+	                        </div>
+	                        <?php while ( have_rows('contact') ) : the_row(); 
+					            $user_id = get_sub_field('contact_person')['ID'];
+					            ?>
+								<address class="contact mb2">
+			                        <div class="contact__img-container">
+			                            <img 
+			                                data-src="<?php echo get_field('profile_image', 'user_'. $user_id)["sizes"]["vh_profile@2x"]; ?>" 
+			                                alt="'Skrivet av: ' + <?php echo get_sub_field('contact_person')['user_firstname'] ?>" 
+			                                class="contact__img lazyload"
+			                            />
+			                        </div>
+			                        <div class="contact__bio">
+			                            <span class="block contact__name"><?php echo get_sub_field('contact_person')['display_name'] ?></span>
+			                            <span class="block contact__title"><?php echo get_field('role', 'user_'. $user_id) ?></span>
+			                            <a class="contact__email" href="mailto:<?php the_author_meta('user_email'); ?>">
+			                            	<?php echo get_sub_field('contact_person')['user_email'] ?>
+
+										</a>
+			                        </div>
+			                    </address>
+	                        <?php endwhile ?>
+	                    </div>
+	                <?php endif ?>
+	            </div>
 			    <div class="col col-12 sm-col-3 md-col-3 page__sidebar">
 	    			<address class="author-horizontal mt4 mb4">
 	                    <div class="author-horizontal__img-container">
