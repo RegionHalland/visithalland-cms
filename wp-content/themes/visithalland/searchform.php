e86e32f2079a7b0fdde66aa315b22dc1747caaf4
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-02-27 14:48:05 +0100

    Remove action #25

--- wp-content/themes/visithalland/searchform.php
+++ wp-content/themes/visithalland/searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform col-12" action="//localhost:3000/">
+<form role="search" method="get" id="searchform" class="searchform col-12">
     <label class="screen-reader-text sr-only col-12" for="s"><?php _e( 'Sök efter', 'visithalland' ); ?>:</label>
     <input type="text" value="" name="s" id="s" class="col-12">
     <input type="submit" id="searchsubmit" value="Sök">
