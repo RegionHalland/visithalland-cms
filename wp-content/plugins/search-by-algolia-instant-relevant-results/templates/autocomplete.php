1e5f1f7faee71d3cefae1d8abb69583fa25fcc41
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-02-28 11:55:19 +0100

    Add generic look to happenings

--- wp-content/plugins/search-by-algolia-instant-relevant-results/templates/autocomplete.php
+++ wp-content/plugins/search-by-algolia-instant-relevant-results/templates/autocomplete.php
@@ -7,7 +7,7 @@
 <script type="text/html" id="tmpl-autocomplete-post-suggestion">
 
 
-  <# if ( data.post_type == 'happening' ) { #>
+  <!-- <# if ( data.post_type == 'happening' ) { #>
 	<article class="happening-list-item mt3 {{{ data.taxonomy.slug }}}">
         <a href="{{ data.permalink }}" title="{{ data.post_title }}" class="link-reset">
             <div class="clearfix">
@@ -43,6 +43,43 @@
             </div>
         </a>
     </article>
+  <# } #> -->
+
+  <# if ( data.post_type == 'happening' ) { #>
+  <a class="suggestion-link" href="{{ data.permalink }}" title="{{ data.post_title }}">
+	<# if ( data.images.thumbnail ) { #>
+	<div class="suggestion clearfix {{{ data.taxonomy.slug }}}">
+	  <div class="col col-4 md-col-4 relative">
+		  <div class="suggestion__img-container topographic-pattern relative">
+		  	<div class="article-tag absolute top-0 left-0 mt1 ml1 z3">
+				<div class="article-tag__icon-wrapper">
+					<div class="article-tag__icon"></div>
+				</div>
+			</div>
+        	<picture>
+        		<img class="suggestion__img lazyload"
+                    data-src="{{ data.images.thumbnail.url }}"
+                    alt="{{ data.post_title }}"
+            	/>
+			</picture>
+        </div>
+	  </div>
+	  <# } #>
+		<div class="col col-8 md-col-8 suggestion__content">
+		  <span class="suggestion__title">{{{ data.post_title }}}</span>
+		  <div class="read-more">
+	    	<span class="read-more__text">
+	    		Läs mer
+	    	</span>
+	    	<div class="read-more__button">
+		    	<svg class="icon read-more__icon">
+                	<use xlink:href="#arrow-right-icon"/>
+            	</svg>
+        	</div>
+	    </div>
+		</div>
+	</div>
+  </a>
   <# } #>
 
 
