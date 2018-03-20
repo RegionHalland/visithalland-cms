9f025abd6497e544a7e231230cd25ceffd0ca363
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-03-20 14:44:56 +0100

    Add grid to taxonomy page

--- web/app/themes/visithalland/resources/assets/scripts/autoload/dynamicgrid.js
+++ web/app/themes/visithalland/resources/assets/scripts/autoload/dynamicgrid.js
@@ -2,12 +2,14 @@ var Packery = require('packery');
 
 class DynamicGrid {
     constructor() {
+
+        var grid = $('.dynamic-grid')[0];
         
-        // var pckry = new Packery($('.grid'), {
-        //   itemSelector: '.grid-item',
-        //   percentPosition: true,
-        //   transitionDuration: 0
-        // });
+        var pckry = new Packery(grid, {
+          itemSelector: '.dynamic-grid__item',
+          percentPosition: true,
+          transitionDuration: 0
+        });
 
     }
 }
