d8a5081b80b6a4c5fd93627c663fb254caad2290
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-03-20 15:24:41 +0100

    Add editorial

---
+++ web/app/themes/visithalland/resources/views/partials/mentions.blade.php
@@ -0,0 +1,51 @@
+@php $mentions = get_field("mentioned"); @endphp
+@if(isset($mentions) && is_array($mentions))
+    <div class="article-mentions mt2 clearfix">
+        <div class="article-mentions__header">
+            <h3>@php _e( 'Restips från artikeln', 'visithalland' ); @endphp</h3>
+        </div>
+        @foreach ($mentions as $key => $mention)
+            <a href="{{ get_permalink($mention->ID) }}" class="link-reset">
+                <article class="article-mention col col-12 sm-col-6 mt3">
+                    <div class="clearfix">
+                        <div class="col col-5 sm-col-4 ">
+                            <div class="article-mention__img-container relative">
+                                @php
+                                    $thumbnail_id = get_post_thumbnail_id( $mention->ID );
+                                    $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+                                @endphp
+                                <picture>
+                                    <source
+                                        data-srcset="{{ get_the_post_thumbnail_url( $mention->ID, 'vh_thumbnail' ) . " 1x," . get_the_post_thumbnail_url( $mention->ID, 'vh_thumbnail@2x' ) . " 2x" }}" />
+                                    <img class="article-mention__img lazyload"
+                                        data-src="{{ get_the_post_thumbnail_url( $mention->ID, 'vh_thumbnail' ) }}"
+                                        alt="{{ $alt }}"
+                                    />
+                                </picture>
+                            </div>
+                        </div>
+                        <div class="article-mention__content col col-7 sm-col-8">
+                            <h4 class="article-mention__title">
+                                @if(get_field("title", $mention->ID) != '')
+                                    {{ the_field("title", $mention->ID) }}
+                                @else
+                                    {{ $mention->post_title }}
+                                @endif
+                            </h4>
+                            <div class="read-more">
+                                <span class="read-more__text">
+                                    @php _e( 'Läs mer', 'visithalland' ); @endphp
+                                </span>
+                                <div class="read-more__button">
+                                    <svg class="icon read-more__icon">
+                                        <use xlink:href="#arrow-right-icon"/>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </a>
+        @endforeach
+    </div>
+@endif
