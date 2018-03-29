9ef039433e5b4e014c8afba36bb1a874332e770d
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-03-29 14:41:27 +0200

    Add working infinite scroll #130

--- web/app/themes/visithalland/resources/assets/scripts/autoload/infinite-scroll.js
+++ web/app/themes/visithalland/resources/assets/scripts/autoload/infinite-scroll.js
@@ -1,14 +1,11 @@
 var inf = require('infinite-scroll');
 
 class InfiniteScroll {
-    // TODO: Test if it works
     constructor(){
         this.bind();
     }
 
-
     bind(){
-        // TODO: make this work ---->
         var nextPages = $("#nextPages").data("all");
         console.log(nextPages);
 
@@ -21,54 +18,17 @@ class InfiniteScroll {
             scrollThreshold: 800,
             history: 'replace',
             debug: true
-            //scrollThreshold: false
         });
 
-        //console.log("nextPages", nextPages);
-
-        /*var infScroll = new InfiniteScroll('#infinite-container', {
-            //path: "http://localhost:3000/camping-friends/spotlight/en-regnig-dag/"
-            //append: '.post',
-            status: '.page-load-status',
-        });
-
-        /*var $container = new InfiniteScroll('#infinite-container', {
-        //var $container = $('#infinite-container').infiniteScroll({
-            // options
-            path: function () {
-                return nextPages[0];
-            },
-            append: '#infinite-container',
-            history: 'replace',
-            status: '.page-load-status',
-            debug: false,
-            scrollThreshold: 800
-        });
-
-        /*var $container = new InfiniteScroll('.container', {
-            // options
-            path: function () {
-                //this.loadCount
-                return nextPages[0];
-            },
-            append: '#infinite-container',
-            history: 'replace',
-            status: '.page-load-status',
-            debug: true,
-            scrollThreshold: 800,
-        });*/
-
-        /*$container.on('load.infiniteScroll', function (event, response) {
-            var elements = $(response);
-        });
-
-        $container.on('history.infiniteScroll', function (event, title, path) {
+        // Add history event listener for logging pageviews to Analytics.
+        infScroll.on('history', function (title, path){
+            console.log("history", title, path);
             ga('create', 'UA-89278649-4');
             ga('send', {
                 hitType: 'pageview',
                 path: path.replace(/^.*\/\/[^\/]+/, ''),
             });
-        });*/
+        });
     }
 
 }
