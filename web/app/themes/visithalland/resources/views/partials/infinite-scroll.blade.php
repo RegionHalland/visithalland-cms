3d86b541a348e9a99ba6108ef01d17e41bc4043d
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-04-03 15:16:59 +0200

    Add infinite scroll feedback

--- web/app/themes/visithalland/resources/views/partials/infinite-scroll.blade.php
+++ web/app/themes/visithalland/resources/views/partials/infinite-scroll.blade.php
@@ -2,12 +2,23 @@
     <div id="nextPages" data-all='{{ json_encode(App::getNextPostLink()) }}'></div>
 </div>
 
-<div class="container">
-    <div class="page-load-status">
-        <div class="infinite-scroll-request">
-        	 <svg class="infinite-scroll-request__icon">
-                <use xlink:href="#arrow-down-icon"/>
-            </svg>
+<div class="page-load-status topographic-pattern">
+    <div class="col-11 md-col-10 lg-col-10 mx-auto">
+        <div class="infinite-scroll-request relative">
+        	<div class="next-article clearfix">
+    			<div class="next-article__img-container inline-block">
+    				<img class="next-article__img" src="" alt="">
+    			</div>
+        		<div class="next-article__content inline-block">
+        			<span class="next-article__heading h5 light">Nästa artikel</span>
+        			<h3 class="next-article__title light">Artikeltitel</h3>
+        		</div>
+        	</div>
+        	<div class="loading-indicator">
+	            <svg class="loading-indicator__icon">
+	                <use xlink:href="#arrow-down-icon"/>
+	            </svg>
+	        </div>
         </div>
         <p class="infinite-scroll-last">End of content</p>
   		<p class="infinite-scroll-error">No more pages to load</p>
