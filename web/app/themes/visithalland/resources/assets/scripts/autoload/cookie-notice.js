40f39597f74699ee797786ab784d69bc6f856b7e
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-04-04 16:07:44 +0200

    Remove unneeded console.logs #130

--- web/app/themes/visithalland/resources/assets/scripts/autoload/cookie-notice.js
+++ web/app/themes/visithalland/resources/assets/scripts/autoload/cookie-notice.js
@@ -7,13 +7,11 @@ class CookieNotice {
 
     bind() {
         // Init Cookie-banner Star
-        console.log(!Cookies.get('cookie_consent'));
         if (!Cookies.get('cookie_consent')) {
             $(".cookie-banner").fadeIn(600);
         };
 
         $('#cookie-accept').on('click', function () {
-            console.log("accpet click")
             Cookies.set('cookie_consent', 'comply', { expires: 5000 });
             $(".cookie-banner").fadeOut(300); //$ to slide it up
         });
