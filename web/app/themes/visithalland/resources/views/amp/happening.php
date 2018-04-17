9e2a8d81c47bd9c3958c5e9a5ab57c6b59f94385
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-04-17 16:37:04 +0200

    Add article-hero to amp articles

--- web/app/themes/visithalland/resources/views/amp/happening.php
+++ web/app/themes/visithalland/resources/views/amp/happening.php
@@ -1,7 +1,17 @@
 <?php include('header.php'); ?>
 
-<h2 class="mt6 px3"><?php the_title(); ?></h2>
-<div class="article-body px3">
+<?php while( have_posts() ) : the_post();
+	$post_id = get_the_ID();
+    $thumbnail_id = get_post_thumbnail_id();
+    $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+    $thumbnail_image = get_posts(array('p' => $thumbnail_id, 'post_type' => 'attachment'));
+endwhile;
+?>
+
+<?php include('article-hero.php'); ?>
+
+<p class="preamble col-11 mx-auto mt4"><?php echo get_field('excerpt'); ?></p>
+<div class="article-body col-11 mx-auto mt3">
 	<?php the_content(); ?>
 </div>
 
