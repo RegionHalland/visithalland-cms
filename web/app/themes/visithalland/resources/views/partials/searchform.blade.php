ade2c8702be62d64174ba26b31566c926b414a42
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-04-18 14:42:21 +0200

    Add line-height and container to searchform

--- web/app/themes/visithalland/resources/views/partials/searchform.blade.php
+++ web/app/themes/visithalland/resources/views/partials/searchform.blade.php
@@ -1,6 +1,6 @@
 <div class="search-header topographic-pattern">
     <div class="col-11 md-col-10 lg-col-8">
-        <form role="search" method="get" id="searchform" class="searchform">
+        <form role="search" method="get" id="searchform" class="searchform container">
             <label class="screen-reader-text sr-only col-12" for="s">@php _e( 'Sök efter', 'visithalland' ) @endphp:</label>
             <input type="text" placeholder="Vad letar du efter?" value="" name="s" id="s" class="searchform__input">
             <button type="submit" class="icon-button icon-button--large ml2">
