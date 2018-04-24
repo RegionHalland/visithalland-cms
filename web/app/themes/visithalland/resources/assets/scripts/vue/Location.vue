a285bb90ccf73f0e6f554d97c472cb9d3a5e8398
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-04-24 10:35:04 +0200

    Add loading state to location button

--- web/app/themes/visithalland/resources/assets/scripts/vue/Location.vue
+++ web/app/themes/visithalland/resources/assets/scripts/vue/Location.vue
@@ -1,22 +1,27 @@
 <template>
 <div>
-    <button class="btn block center btn--primary" v-on:click="askForLocation()">Ja ni får använda min plats</button>
-    <router-link :to="{name: 'results', params: {input: {userLocation: {lat: 2323, lng: 231312}}}}"></router-link>
+    <button class="btn block center btn--primary" :class="{ loading: loading }" v-on:click="askForLocation()">Ja ni får använda min plats</button>
+
+    <router-link :to="{name: 'time', params: {input: {userLocation: {lat: 56.973735, lng: 12.582737}}}}">Gå vidare utan plats</router-link>
 </div>
 </template>
 
 <script>
     export default {
         props: ["input"],
+        data: () => {
+            return {
+                loading: false
+            }
+        },
         methods: {
             askForLocation () {
-                //var vm = this;
-                //return this.$router.push({ name: "time", params: {input: {userLocation: {lat: "56.6689603", lng: "12.8567978" }} }});
-
+                this.loading = true;
                 this.$getLocation()
                     .then(coordinates => {
                         console.log(coordinates);
                         // TODO: replace object below with coordinates var
+                        this.loading = false;
                         return this.$router.push({ name: "time", params: {input: {userLocation: {lat: coordinates.lat, lng: coordinates.lng }} }});
                     });
             }
