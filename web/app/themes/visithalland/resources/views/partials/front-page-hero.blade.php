0bae36922766e92ac94ca6fea70455e198a1b091
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-04-25 09:44:28 +0200

    Cleanup main.scss

--- web/app/themes/visithalland/resources/views/partials/front-page-hero.blade.php
+++ web/app/themes/visithalland/resources/views/partials/front-page-hero.blade.php
@@ -1,12 +1,12 @@
 <div class="landing-header relative">
     <div class="landing-header__img-container">
         <span class="landing-header__logo center">
-            <img src="@asset('images/landing-logo.svg')">
+            <img class="landing-header__logo" src="@asset('images/landing-logo.svg')">
         </span>
         <picture>
             <source media="(min-width: 60em)" data-srcset="{{ get_the_post_thumbnail_url($post, 'vh_hero_wide') . " 1x," . get_the_post_thumbnail_url($post, 'vh_hero_wide@2x' ) . " 2x"  }}" />
             <source data-srcset="{{ get_the_post_thumbnail_url($post, 'vh_hero_tall') . " 1x," . get_the_post_thumbnail_url($post, 'vh_hero_tall@2x') . " 2x"  }}" />
-            <img class="concept-header__img lazyload"
+            <img class="landing-header__img lazyload"
                 data-src="{{ get_the_post_thumbnail_url($post, 'vh_hero_wide') }}"
                 alt="{{ get_field("cover_image", $post->ID)["alt"] }}" />
         </picture>
