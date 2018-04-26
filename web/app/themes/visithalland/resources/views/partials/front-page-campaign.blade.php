4fa6d827fc06d44b8fca8847855ee1374545b2dd
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-04-26 11:08:00 +0200

    Fix wrong img src

--- web/app/themes/visithalland/resources/views/partials/front-page-campaign.blade.php
+++ web/app/themes/visithalland/resources/views/partials/front-page-campaign.blade.php
@@ -28,8 +28,8 @@
             <source media="(min-width: 60em)" data-srcset="{{ get_field('campaign_img')['sizes']['vh_hero_wide'] . " 1x," . get_field('campaign_img')['sizes']['vh_hero_wide@2x'] . " 2x"  }}" />
             <source data-srcset="{{ get_field('campaign_img')['sizes']['vh_hero_tall'] . " 1x," . get_field('campaign_img')['sizes']['vh_hero_tall@2x'] . " 2x"  }}" />
             <img class="concept-header__img lazyload"
-                data-src="{{ get_the_post_thumbnail_url($post, 'vh_hero_wide') }}"
-                alt="{{ get_field("cover_image", $post->ID)["alt"] }}" />
+                data-src="{{ get_field('campaign_img')['sizes']['vh_hero_wide']}}"
+                alt="{{ get_field('campaign_img')["alt"] }}" />
         </picture>
     </div>
 </div>
