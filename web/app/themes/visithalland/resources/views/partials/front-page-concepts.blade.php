3324195cd3c27f111a650e6cc170529d2dd3dead
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-05-28 14:17:54 +0200

    Test commit to test cache #131

--- web/app/themes/visithalland/resources/views/partials/front-page-concepts.blade.php
+++ web/app/themes/visithalland/resources/views/partials/front-page-concepts.blade.php
@@ -13,6 +13,9 @@
             <div class="section-header__title">
                 @php _e( 'Upptäck det bästa av Halland', 'visithalland' ); @endphp
             </div>
+            <div class="section-header__title">
+                @php _e( 'Upptäck det bästa av Halland', 'visithalland' ); @endphp
+            </div>
         </header>
         <div class="featured-grid clearfix">
             @php $primary_navigation_items = App::getPrimaryNavigation() @endphp
