ffee2b25141bb30795a8d8d9426e89d7a304df06
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-06-05 14:37:11 +0200

    Add comma...

--- web/app/themes/visithalland/app/Api/Callbacks.php
+++ web/app/themes/visithalland/app/Api/Callbacks.php
@@ -34,7 +34,7 @@ function vh_get_events_happenings_by_date(WP_REST_Request $request)
 {
     $date = $request['date'];
     var_dump($date);
-    var_dump($request->get_param( 'date' ))
+    var_dump($request->get_param( 'date' ));
     if(!isset($date)) return new WP_Error('unknown-errorsss', __('Unknown errorsss.', 'visithalland'), array( 'status' => 400 ));
     $compareDate = date("Y-m-d", strtotime($date));
     $events_happenings_array = array();
