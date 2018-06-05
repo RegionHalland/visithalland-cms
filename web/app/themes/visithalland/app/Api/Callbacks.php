db1c29e8365355aae89302bbee6721880ede35d6
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-06-05 17:11:24 +0200

    More debug...

--- web/app/themes/visithalland/app/Api/Callbacks.php
+++ web/app/themes/visithalland/app/Api/Callbacks.php
@@ -33,6 +33,7 @@ function vh_get_location_by_coordinates(WP_REST_Request $request)
 function vh_get_events_happenings_by_date(WP_REST_Request $request)
 {
     $date = $request['date'];
+    var_dump($date);
     var_dump($request);
     //var_dump($request->get_param( 'date' ));
     if(!isset($date)) return new WP_Error('unknown-errorsss', __('Unknown errorsss.', 'visithalland'), array( 'status' => 400 ));
