ef5c021f700511bc63062e2ad9cff437f5f04a9c
David Öhlin <hello@davidohl.in>
2018-06-07 13:05:07 +0200

    wip: timeline

--- web/app/themes/visithalland/resources/views/single-weekend.blade.php
+++ web/app/themes/visithalland/resources/views/single-weekend.blade.php
@@ -1,22 +1,34 @@
 @extends('layouts.landing')
 
 @section('content')
-	@php
-	$timeline = get_field('timeline');
-	@endphp
+<div class="container mb5">
+	@php($timeline = get_field('timeline'))
 
 	@foreach ($timeline as $day)
-	<div>
+	<div class="flex flex-wrap">
 		<div>
 			<span>{{ $day['date'] }}</span>
 		</div>
-		@if(isset($day['content']))
-		<div>
-		@foreach ($day['content'] as $item)
-			<span>hey</span>
-		@endforeach
+		@if(isset($day['content']) && is_array($day['content']))
+		<div class="flex-auto pl4">
+			@foreach ($day['content'] as $item)
+				@if ($item['acf_fc_layout'] === 'post')
+					@php($current = $item['post'])
+					<h2>{{ $current->post_title }}</h2>
+					<p>{{ $current->post_excerpt }}</p>
+					<a class="btn btn-primary" href="{{ get_permalink($current->ID) }}">Läs mer</a>
+				@endif
+				@if ($item['acf_fc_layout'] === 'link')
+					@php($current = $item['link'])
+					<h2>{{ $current['title'] }}</h2>
+					<p>{{ $current['description'] }}</p>
+					<a class="btn btn-primary" href="{{ $current['link'] }}">Läs mer</a>
+				@endif
+			@endforeach
 		</div>
 		@endif
 	</div>
 	@endforeach
+
 @endsection
+</div>
