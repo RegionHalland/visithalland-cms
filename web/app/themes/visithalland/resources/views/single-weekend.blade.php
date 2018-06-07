ed7c29111a7273a48d9468e761d6a331447288f9
David Öhlin <hello@davidohl.in>
2018-06-07 17:11:23 +0200

    wip: "weekend"

--- web/app/themes/visithalland/resources/views/single-weekend.blade.php
+++ web/app/themes/visithalland/resources/views/single-weekend.blade.php
@@ -3,54 +3,65 @@
 @section('content')
 <div class="container mb5">
 	<div class="clearfix">
-	@php($timeline = get_field('timeline'))
-
-	@foreach ($timeline as $day)
-		<div class="col col-12">
-			<div class="col col-12 px3 mb3">
-				<span>{{ $day['date_start'] }}</span>
-				<span>{{ $day['date_end'] }}</span>
-				@php($date = new DateTime($day['date_start']))
-				<span>Vecka {{ $date->format("W") }}</span>
+		<div class="col col-12 flex flex-wrap">
+		@php($timeline = get_field('timeline'))
+		@foreach ($timeline as $day)
+			<div class="col col-12">
+				<div class="col col-12 px3 mb3">
+					<span>{{ $day['date_start'] }}</span>
+					<span>{{ $day['date_end'] }}</span>
+					@php($date = new DateTime($day['date_start']))
+					<span>Vecka {{ $date->format("W") }}</span>
+				</div>
 			</div>
-		</div>
 
-		@if(isset($day['content']) && is_array($day['content']))
-			@foreach ($day['content'] as $item)
-				
-				@if ($item['acf_fc_layout'] === 'post')
-				@php($current = $item['post'])
-
-				<div class="col col-12 md-col-6 px3 mb3">
-					<div>
-						<div class="img-container">
-							<picture>
-	            				<source
-									data-srcset="{{ get_the_post_thumbnail_url( $current->ID, 'vh_large' ) . " 1x," . get_the_post_thumbnail_url( $current->ID, 'vh_large@2x' ) . " 2x" }}" />
-	              				<img class="lazyload"
-									 data-src="{{ get_the_post_thumbnail_url( $current->ID, 'vh_large' ) }}"
-									 alt="{{ get_field("cover_image")["alt"] }}"
-								/>
-	                    	</picture>
+			@if(isset($day['content']) && is_array($day['content']))
+				@foreach ($day['content'] as $item)
+					
+					@if ($item['acf_fc_layout'] === 'post')
+					@php($current = $item['post'])
+					<div class="col col-12 md-col-6 px3 mb3">
+						<div>
+							<div class="weekend__img-container mb3">
+								<picture>
+		            				<source
+										data-srcset="{{ get_the_post_thumbnail_url( $current->ID, 'vh_medium' ) . " 1x," . get_the_post_thumbnail_url( $current->ID, 'vh_medium@2x' ) . " 2x" }}" />
+		              				<img class="lazyload"
+										 data-src="{{ get_the_post_thumbnail_url( $current->ID, 'vh_medium' ) }}"
+										 alt="{{ get_field("cover_image")["alt"] }}"
+									/>
+		                    	</picture>
+							</div>
+							<h2><a href="{{ get_permalink($current->ID) }}">{{ $current->post_title }}</a></h2>
+							<p>{{ $current->post_excerpt }}</p>
 						</div>
-					<a href="{{ get_permalink($current->ID) }}"><h2>{{ $current->post_title }}</h2></a>
-					<p>{{ $current->post_excerpt }}</p>
 					</div>
-				</div>
-				@endif
+					@endif
 
-{{-- 				@if ($item['acf_fc_layout'] === 'link')
-				@php($current = $item['link'])
-				<div class="col col-12 md-col-6 px3 mb3">
-					<a href="{{ $current['link'] }}"><h2>{{ $current['title'] }}</h2></a>
-					<p>{{ $current['description'] }}</p>
-				</div>
-				@endif --}}
-				
-			@endforeach
-		@endif
+					@if ($item['acf_fc_layout'] === 'link')
+					<div class="col col-12 md-col-6 px3 mb3">
+						<div>
+							<div class="weekend__img-container mb3">
+								<picture>
+		            				<source
+										data-srcset="{{ $item['image']['sizes']['vh_medium'] . " 1x," . $item['image']['sizes']['vh_medium@2x'] . " 2x" }}" />
+		              				<img class="lazyload"
+										 data-src="{{ $item['image']['sizes']['vh_medium'] }}"
+										 alt=""
+									/>
+		                    	</picture>
+							</div>
+							<h2><a href="{{ $item['url'] }}">{{ $item['title'] }}</a></h2>
+							<p>{{ $item['description'] }}</p>
+						</div>
+					</div>
+					@endif
+					
+				@endforeach
+			@endif
 
-	@endforeach
+		@endforeach
+		</div>
 	</div>
 </div>
 @endsection
