5e127ae7dc71a3b48a01ea0917a07a11f38ffdd9
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-06-07 13:32:19 +0200

    Add skördetids aktiviteter acf field #191

--- web/app/themes/visithalland/app/Acf/php/skordetids-aktiviteter_group_5b0ffc9a39c66.php
+++ web/app/themes/visithalland/app/Acf/php/skordetids-aktiviteter_group_5b0ffc9a39c66.php
@@ -3,7 +3,7 @@
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group(array(
     'key' => 'group_5b0ffc9a39c66',
-    'title' => 'Skördetids-aktiviteter',
+    'title' => __('Skördetids-aktiviteter', 'visithalland'),
     'fields' => array(
         0 => array(
             'key' => 'field_5b1001b2bea79',
