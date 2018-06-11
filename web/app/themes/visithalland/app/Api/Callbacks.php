6d9d055f1b43b408b21334a2f11cb4b587c1bd77
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-06-11 16:23:52 +0200

    Remove var dump

--- web/app/themes/visithalland/app/Api/Callbacks.php
+++ web/app/themes/visithalland/app/Api/Callbacks.php
@@ -33,10 +33,8 @@ function vh_get_location_by_coordinates(WP_REST_Request $request)
 function vh_get_events_happenings_by_date(\WP_REST_Request $request)
 {
     $date = $request['date'];
-    var_dump($date);
-    var_dump($request);
-    //var_dump($request->get_param( 'date' ));
-    if(!isset($date)) return new WP_Error('unknown-errorsss', __('Unknown errorsss.', 'visithalland'), array( 'status' => 400 ));
+    if(!isset($date)) return new WP_Error('unknown-error', __('Unknown error.', 'visithalland'), array( 'status' => 400 ));
+    
     $compareDate = date("Y-m-d", strtotime($date));
     $events_happenings_array = array();
 
