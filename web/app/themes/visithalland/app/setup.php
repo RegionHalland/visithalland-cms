7f4a51e24125e8ac19fb06af23afc086d1d85d0f
David Öhlin <hello@davidohl.in>
2018-06-14 12:12:46 +0200

    enqueue skördetider css :corn:

--- web/app/themes/visithalland/app/setup.php
+++ web/app/themes/visithalland/app/setup.php
@@ -22,7 +22,8 @@ add_action('wp_enqueue_scripts', function () {
         wp_enqueue_script('sage/amp.js', asset_path('scripts/amp.js'), [], null, true);
         //wp_enqueue_style('sage/amp.css', asset_path('styles/amp.css'), false, null);
     } else{
-        if (basename(get_page_template()) != "template-activities.blade.php") {
+        if (basename(get_page_template()) != "template-activities.blade.php" && 
+            basename(get_page_template()) != "template-landing-skordetider.blade.php") {
             wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
             wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
         }
@@ -30,6 +31,11 @@ add_action('wp_enqueue_scripts', function () {
         if(basename(get_page_template()) == "template-activities.blade.php") {
             wp_enqueue_script('sage/vue.js', asset_path('scripts/vue.js'), null, null, true);
             wp_enqueue_style('sage/vue.css', asset_path('styles/vue.css'), false, null);
+        }
+
+        if (basename(get_page_template()) == "template-landing-skordetider.blade.php") {
+            wp_enqueue_script('sage/skordetider.js', asset_path('scripts/skordetider.js'), ['jquery'], null, true);
+            wp_enqueue_style('sage/skordetider.css', asset_path('styles/skordetider.css'), false, null);
         }
     }
 
