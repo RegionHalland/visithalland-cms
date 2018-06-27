886ebbb03a2ba79e7cc809e7eab32b2748af6858
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-06-27 11:11:39 +0200

    Add border to articles and working dates

--- web/app/themes/visithalland/resources/views/partials/st-week.blade.php
+++ web/app/themes/visithalland/resources/views/partials/st-week.blade.php
@@ -6,19 +6,26 @@
 			<div class="clearfix flex flex-wrap">
 				<div class="st-timeline col col-12 sm-col-2">
 					<div class="st-timeline__badge">
-						<div class="date-badge">
+						<div class="date-badge flex items-center justify-center">
 							@php
 								$dateStart = new DateTime($week['date_start']);
 								$dateEnd = new DateTime($week['date_end']);
 							@endphp
-						    <span class="date-badge__day">{{ $dateStart->format('d') }}-{{ $dateEnd->format('d') }}</span>
-						    <span class="date-badge__month">{{ $dateEnd->format('M') }}</span>
+							<div class="inline-block">
+						    	<span class="date-badge__day">{{ $dateStart->format('d') }}</span>
+						    	<span class="date-badge__month">{{ $dateStart->format('M') }}</span>
+							</div>
+							<div class="inline-block px1">-</div>
+							<div class="inline-block">
+						    	<span class="date-badge__day">{{ $dateEnd->format('d') }}</span>
+						    	<span class="date-badge__month">{{ $dateEnd->format('M') }}</span>
+						    </div>
 						</div>
 					</div>
 					<div class="st-timeline__line first"></div>
 				</div>
-				<div class="st-week-header__content  col col-12 sm-col-8 lg-col-5">
-					<h2 class="st-week-header__title mb3">{{ $week['title'] }}</h2>
+				<div class="st-week-header__content  col col-12 sm-col-8 lg-col-7">
+					<h2 class="st-week-header__title mb4">{{ $week['title'] }}</h2>
 					<p class="st-week-header__description mb3">{{ $week['description'] }}</p>
 				</div>
 			</div>
@@ -29,16 +36,16 @@
 				<div class="st-timeline col col-12 sm-col-2">
 					<div class="st-timeline__line"></div>
 				</div>
-				<div class="col col-12 sm-col-10 mb4 st-week-grid">
+				<div class="col col-12 sm-col-10 lg-col-9 mb4 st-week-grid">
 					@foreach ($week['content'] as $content)
 						
 						{{-- POST --}}
 						@if ($content['acf_fc_layout'] === 'post' && $content['fullsize'] != true)
 						
-						<div class="st-week-grid__item col col-12 sm-col-6 mb5">
+						<div class="st-week-grid__item col col-12 sm-col-6 mb4">
 							<a href="{{ get_permalink($content['post']->ID) }}" title="{{ $content['post']->post_title }}" class="link-reset">
-	                            <article class="article-medium relative {{ App::getTermClassName() }}">
-	                                    <div class="article-medium__img-container relative topographic-pattern">
+	                            <article class="article relative {{ App::getTermClassName() }}">
+	                                    <div class="article__img-container relative topographic-pattern">
 	                                        <div class="article-tag mt2 ml2 absolute z3 left-0 top-0">
 	                                            <div class="article-tag__icon-wrapper">
 	                                                <div class="article-tag__icon"></div>
@@ -51,15 +58,15 @@
 	                                        <picture>
 	                                            <source
 	                                                data-srcset="{{ get_the_post_thumbnail_url( $content['post']->ID, 'vh_medium' ) . " 1x," . get_the_post_thumbnail_url( $content['post']->ID, 'vh_medium@2x' ) . " 2x" }}" />
-	                                            <img class="article-medium__img lazyload"
+	                                            <img class="article__img lazyload"
 	                                                data-src="{{ get_the_post_thumbnail_url( $content['post']->ID, 'vh_medium' ) }}"
 	                                                alt="{{ $alt }}"
 	                                            />
 	                                        </picture>
 	                                    </div>
-	                                    <div class="article-medium__content mt3 {{ App::getTermClassName() }}">
-	                                        <h3 class="article-medium__title mb1 mt1 pt0">{{ $content['post']->post_title }}</h3>
-	                                        <p class="article-medium__excerpt mt2">@php the_field("excerpt", $content['post']->ID) @endphp</p>
+	                                    <div class="article__content {{ App::getTermClassName() }}">
+	                                        <h3 class="article__title mb1 mt1 pt0">{{ $content['post']->post_title }}</h3>
+	                                        <p class="article__excerpt mt2">@php the_field("excerpt", $content['post']->ID) @endphp</p>
 	                                        <div class="read-more my3">
 	                                            <span class="read-more__text">
 	                                                @php _e( 'Läs mer', 'visithalland' ); @endphp
@@ -80,11 +87,12 @@
 
 						{{-- POST FULLSIZE --}}
 						@if ($content['acf_fc_layout'] === 'post' && $content['fullsize'] === true)
-						<div class="st-week-grid__item col col-12 mb5">
+						<div class="st-week-grid__item col col-12 mb4">
 							<a href="{{ get_permalink($content['post']->ID) }}" title="{{ $content['post']->post_title }}" class="link-reset">
-	                            <article class="article-medium relative mxn3 {{ App::getTermClassName() }}">
-	                            	<div class="col col-12 sm-col-6 px3">
-	                                    <div class="article-medium__img-container relative topographic-pattern">
+	                            <article class="article relative {{ App::getTermClassName() }}">
+	                            	<div class="mxn2">
+	                            	<div class="col col-12 sm-col-6 px2">
+	                                    <div class="article__img-container relative topographic-pattern">
 	                                        <div class="article-tag mt2 ml2 absolute z3 left-0 top-0">
 	                                            <div class="article-tag__icon-wrapper">
 	                                                <div class="article-tag__icon"></div>
@@ -97,17 +105,17 @@
 	                                        <picture>
 	                                            <source
 	                                                data-srcset="{{ get_the_post_thumbnail_url( $content['post']->ID, 'vh_medium' ) . " 1x," . get_the_post_thumbnail_url( $content['post']->ID, 'vh_medium@2x' ) . " 2x" }}" />
-	                                            <img class="article-medium__img lazyload"
+	                                            <img class="article__img lazyload"
 	                                                data-src="{{ get_the_post_thumbnail_url( $content['post']->ID, 'vh_medium' ) }}"
 	                                                alt="{{ $alt }}"
 	                                            />
 	                                        </picture>
 	                                    </div>
 	                                </div>
-	                                <div class="col col-12 sm-col-6 px3">
-	                                    <div class="article-medium__content mt3 {{ App::getTermClassName() }}">
-	                                        <h3 class="article-medium__title mb1 mt1 pt0">{{ $content['post']->post_title }}</h3>
-	                                        <p class="article-medium__excerpt mt2">@php the_field("excerpt", $content['post']->ID) @endphp</p>
+	                                <div class="col col-12 sm-col-6 px2">
+	                                    <div class="article__content mt3 {{ App::getTermClassName() }}">
+	                                        <h3 class="article__title mb1 pt0">{{ $content['post']->post_title }}</h3>
+	                                        <p class="article__excerpt mt2">@php the_field("excerpt", $content['post']->ID) @endphp</p>
 	                                        <div class="read-more my3">
 	                                            <span class="read-more__text">
 	                                                @php _e( 'Läs mer', 'visithalland' ); @endphp
@@ -120,6 +128,7 @@
 	                                        </div>
 	                                    </div>
 	                                </div>
+	                                </div>
 	                            </article>
 	                        </a>
 						</div>
@@ -128,10 +137,10 @@
 
 						{{-- LINK --}}
 						@if ($content['acf_fc_layout'] === 'link' && $content['fullsize'] != true)
-						<div class="st-week-grid__item col col-12 {{ $content['fullsize'] === true ? '' : 'sm-col-6' }} mb5">
+						<div class="st-week-grid__item col col-12 {{ $content['fullsize'] === true ? '' : 'sm-col-6' }} mb4">
 						    <a href="{{ $content['url'] }}" title="{{ $content['title']}}" class="link-reset">
-						        <article class="article-medium relative {{ App::getTermClassName() }}">
-						                <div class="article-medium__img-container relative topographic-pattern">
+						        <article class="article relative {{ App::getTermClassName() }}">
+						                <div class="article__img-container relative topographic-pattern">
 						                    <div class="article-tag mt2 ml2 absolute z3 left-0 top-0">
 						                        <div class="article-tag__icon-wrapper">
 						                            <div class="article-tag__icon"></div>
@@ -144,15 +153,15 @@
 						                    <picture>
 						                        <source
 						                            data-srcset="{{ $content['image']['sizes']['vh_medium'] . " 1x," . $content['image']['sizes']['vh_medium@2x'] . " 2x" }}" />
-						                        <img class="article-medium__img lazyload"
+						                        <img class="article__img lazyload"
 						                            data-src="{{ $content['image']['sizes']['vh_medium'] }}"
 						                            alt="{{ get_field("cover_image")["alt"] }}"
 						                        />
 						                    </picture>
 						                </div>
-						                <div class="article-medium__content mt3 {{ App::getTermClassName() }}">
-						                    <h3 class="article-medium__title mb1 mt1 pt0">{{ $content['title'] }}</h3>
-						                    <p class="article-medium__excerpt mt2">{{ $content['description'] }}</p>
+						                <div class="article__content {{ App::getTermClassName() }}">
+						                    <h3 class="article__title mb1 mt1 pt0">{{ $content['title'] }}</h3>
+						                    <p class="article__excerpt mt2">{{ $content['description'] }}</p>
 						                    <div class="read-more my3">
 						                        <span class="read-more__text">
 						                            @php _e( 'Läs mer', 'visithalland' ); @endphp
@@ -172,11 +181,11 @@
 
 						{{-- LINK FULLSIZE --}}
 						@if ($content['acf_fc_layout'] === 'link' && $content['fullsize'] === true)
-						<div class="st-week-grid__item col col-12 mb5">
+						<div class="st-week-grid__item col col-12 mb4">
 							<a href="{{ $content['url'] }}" title="{{ $content['title']}}" class="link-reset">
-						        <article class="article-medium relative {{ App::getTermClassName() }} mxn3">
+						        <article class="article relative {{ App::getTermClassName() }} mxn3">
 						        	<div class="col col-12 sm-col-6 px3">
-						                <div class="article-medium__img-container relative topographic-pattern">
+						                <div class="article__img-container relative topographic-pattern">
 						                    <div class="article-tag mt2 ml2 absolute z3 left-0 top-0">
 						                        <div class="article-tag__icon-wrapper">
 						                            <div class="article-tag__icon"></div>
@@ -189,7 +198,7 @@
 						                    <picture>
 						                        <source
 						                            data-srcset="{{ $content['image']['sizes']['vh_medium'] . " 1x," . $content['image']['sizes']['vh_medium@2x'] . " 2x" }}" />
-						                        <img class="article-medium__img lazyload"
+						                        <img class="article__img lazyload"
 						                            data-src="{{ $content['image']['sizes']['vh_medium'] }}"
 						                            alt="{{ get_field("cover_image")["alt"] }}"
 						                        />
@@ -197,9 +206,9 @@
 						                </div>
 						            </div>
 						            <div class="col col-12 sm-col-6 px3">
-						                <div class="article-medium__content mt3 {{ App::getTermClassName() }}">
-						                    <h3 class="article-medium__title mb1 mt1 pt0">{{ $content['title'] }}</h3>
-						                    <p class="article-medium__excerpt mt2">{{ $content['description'] }}</p>
+						                <div class="article__content mt3 {{ App::getTermClassName() }}">
+						                    <h3 class="article__title mb1 mt1 pt0">{{ $content['title'] }}</h3>
+						                    <p class="article__excerpt mt2">{{ $content['description'] }}</p>
 						                    <div class="read-more my3">
 						                        <span class="read-more__text">
 						                            @php _e( 'Läs mer', 'visithalland' ); @endphp
@@ -220,7 +229,7 @@
 
 						{{-- QUOTE --}}
 						@if ($content['acf_fc_layout'] === 'quote')
-						<div class="st-week-grid__item col col-12 mb5">
+						<div class="st-week-grid__item col col-12 mb4">
 							<div class="st-week-quote">
 								<div class="st-week-quote__img-container">
 									<picture>
@@ -244,7 +253,7 @@
 
 						{{-- CAROUSEL --}}
 						@if ($content['acf_fc_layout'] === 'carousel')
-						<div class="st-week-grid__item col col-12 mb5">
+						<div class="st-week-grid__item col col-12 mb4">
 							<h2 class="st-week-carousel__title mb3">{{ $content['description'] }}</h2>
 							<div class="relative">
 								<button class="st-carousel-previous icon-button">
