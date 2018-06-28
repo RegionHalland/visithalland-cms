deb8fc7b94e4c0baf1fe77f0e9552f47a898e98c
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-06-28 16:47:28 +0200

    Add working logo to landing

--- web/app/themes/visithalland/resources/views/st-template-landing.blade.php
+++ web/app/themes/visithalland/resources/views/st-template-landing.blade.php
@@ -7,8 +7,11 @@
 @section('content')
 
 <header class="st-list__header flex items-center">
-	<div class="container col-11 sm-col-8 md-col-7 lg-col-6 mx-auto relative">
-		<h1 class="st-list__title center">{{ the_title() }}</h1>
+	<div class="container col-11 sm-col-8 md-col-7 lg-col-6 mx-auto center relative">
+		<img
+			class="st-list__title"
+			src="@asset('images/st-logo-large.svg')"
+			alt="Visithalland.com" />
 	</div>
 </header>
 	
