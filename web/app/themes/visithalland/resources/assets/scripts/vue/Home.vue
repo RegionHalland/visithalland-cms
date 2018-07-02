ebce95edd88ce0c3e6a287a3bd0e4f9493344f25
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-07-02 08:41:18 +0200

    Add new styles to start screen :+1:

--- web/app/themes/visithalland/resources/assets/scripts/vue/Home.vue
+++ web/app/themes/visithalland/resources/assets/scripts/vue/Home.vue
@@ -1,17 +1,21 @@
 <i18n>
   {
     "en": {
-      "buttonTxt": "Get started"
+      "buttonTxt": "Get started",
+      "introTxt": "A day in Halland helps you find interesting spots."
     },
     "sv": {
-      "buttonTxt": "Få inspiration"
+      "buttonTxt": "Starta nu",
+      "introTxt": "A day in Halland hjälper dig hitta riktigt nice saker att göra när du semestrar och sådär du vet."
     }
   }
 </i18n>
 
 <template>
-    <div>
-        <router-link class="btn block center btn--primary" to="location">{{ $t('buttonTxt') }}</router-link>
+    <div class="dh-intro center">
+        <h1 class="light">A Day in Halland</h1>
+        <p class="light my3">{{ $t('introTxt') }}</p>
+        <router-link class="btn block center btn--primary dh-button" to="location">{{ $t('buttonTxt') }}</router-link>
 
         <CookieNotice />
     </div>
