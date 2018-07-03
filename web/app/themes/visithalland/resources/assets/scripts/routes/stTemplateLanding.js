b9af06ccb10bfb9ad2e6d2ac44d7f546fe9f312d
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-07-03 11:17:55 +0200

    Revert "Add scrollTop function to cta"
    This reverts commit 802d996f4fbc887c8ce14d3ce8ddbbe5ce6d4f03.

--- web/app/themes/visithalland/resources/assets/scripts/routes/stTemplateLanding.js
+++ web/app/themes/visithalland/resources/assets/scripts/routes/stTemplateLanding.js
@@ -5,13 +5,6 @@ export default {
         $(document).ready(() => {
             let $carousels = $('.js-carousel');
             initFlickity($carousels);
-
-            $('.st-cta-button').click(function(){
-            	console.log("Scrolla");
-            	$('html, body').animate({
-			        scrollTop: $(".st-week-header").offset().top
-			    }, 500);
-            })
         });
     },
 };
