421c1113edd62de4b1888e266c1e58128e62f8bf
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-07-04 10:56:49 +0200

    Add partners and correct copy for sections and buttons

--- web/app/themes/visithalland/resources/views/partials/st-carousel.blade.php
+++ web/app/themes/visithalland/resources/views/partials/st-carousel.blade.php
@@ -7,7 +7,7 @@
 	src="@asset('images/st-beet.svg')"
 	alt="Visithalland.com" />
 	<div class="container py5 col-11 md-col-10 lg-col-10 mx-auto relative">
-		<h2 class="mb3 h3 st-carousel__title">Upptäck Halland i skördetid</h2>
+		<h2 class="mb3 h3 st-carousel__title">Mer av skördetid i Halland</h2>
 		<div class="relative js-carousel-parent">
 			<button class="st-carousel-previous js-carousel-previous icon-button">
 				<svg class="icon--sm icon-button__icon">
