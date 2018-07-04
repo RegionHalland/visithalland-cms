1c16cfaedf17a6a0d1e311139769114cfff5f2ab
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-07-04 08:23:00 +0200

    Add wordmark sizing

--- web/app/themes/visithalland/resources/views/st-template-landing.blade.php
+++ web/app/themes/visithalland/resources/views/st-template-landing.blade.php
@@ -11,7 +11,7 @@
 <header class="st-list__header flex items-center">
 	<div class="container col-11 sm-col-8 md-col-7 lg-col-6 mx-auto center relative">
 		<img
-			class="st-list__title"
+			class="st-list__img"
 			src="@asset('images/st-logo-large.svg')"
 			alt="Visithalland.com" />
 			<div class="col-12 sm-col-8 md-col-6 flex items-center mx-auto mt5">
