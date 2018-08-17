1fa7f8d9087a9057932e68876bfebd16b061c74e
David Öhlin <hello@davidohl.in>
2018-08-17 09:35:27 +0200

    add eu-jordbruksfonden logo :8ball:

--- web/app/themes/visithalland/resources/views/partials/st-partners.blade.php
+++ web/app/themes/visithalland/resources/views/partials/st-partners.blade.php
@@ -1,18 +1,21 @@
 <section class="st-partners">
 	<div class="container py4 col-11 md-col-10 lg-col-10 mx-auto relative center">
 		<h2 class="st-partners__title mt3">I samarbete med</h2>
-		<div class="st-partners__inner mxn2 clearfix py4 flex flex-wrap">
-			<div class="col col-12 sm-col-6 md-col-3 center px2 py3">
+		<div class="st-partners__inner mxn2 clearfix py4 flex flex-wrap justify-center">
+			<div class="col col-10 sm-col-6 md-col-2 flex items-center justify-center px2 py3">
 				<img class="st-partner" src="@asset('images/signerat-halland-logo.png')" alt="">
 			</div>
-			<div class="col col-12 sm-col-6 md-col-3 center px2 py3">
+			<div class="col col-10 sm-col-6 md-col-2 flex items-center justify-center px2 py3">
 				<img class="st-partner px2" src="@asset('images/regionhalland-logo.png')" alt="">
 			</div>
-			<div class="col col-12 sm-col-6 md-col-3 center px2 py3">
+			<div class="col col-10 sm-col-6 md-col-2 flex items-center justify-center px2 py3">
 				<img class="st-partner px2" src="@asset('images/hallands-matgille-logo.png')" alt="">
 			</div>
-			<div class="col col-12 sm-col-6 md-col-3 center px2 py3">
+			<div class="col col-10 sm-col-6 md-col-2 flex items-center justify-center px2 py3">
 				<img class="st-partner px2" src="@asset('images/lrf-logo.png')" alt="">
+			</div>
+			<div class="col col-10 sm-col-6 md-col-2 flex items-center justify-center px2 py3">
+				<img class="st-partner px2" src="@asset('images/eu-jordbruksfonden.jpg')" alt="">
 			</div>
 		</div>
 	</div>
