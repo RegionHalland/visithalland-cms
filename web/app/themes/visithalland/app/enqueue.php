9f867ea57381364035f93dbf69118793443773c1
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-09-10 14:30:24 +0200

    Add new Google Maps key #220

--- web/app/themes/visithalland/app/enqueue.php
+++ web/app/themes/visithalland/app/enqueue.php
@@ -7,5 +7,5 @@ add_action('wp_enqueue_scripts', function()
     wp_dequeue_style('algolia-autocomplete');
     wp_dequeue_style('algolia-instantsearch');
 
-    wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCwAJMUt4ODfivqIGMgeZeTlH90-QqRFrU&libraries=places', 'jquery', false, true);
+    wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBzfYgyGwvbQi9oq1ydDRfNbLm5LZ9qVHw&libraries=places', 'jquery', false, true);
 }, 20);
