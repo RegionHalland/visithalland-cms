6bcec915f5531a4e95e9f12e20c5595056aa4146
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-09-17 13:16:28 +0200

    Add new  styles for infinite scroll indicator

--- web/app/themes/visithalland/resources/views/partials/components/next-article.blade.php
+++ web/app/themes/visithalland/resources/views/partials/components/next-article.blade.php
@@ -1,4 +1,4 @@
-<div class="next-article__container topographic-pattern">
+<div class="next-article__container topographic-pattern bg-blue-light">
 	<div class="container col-11 md-col-10 lg-col-10 mx-auto">
 	    <div class="infinite-scroll-request relative">
 	    	<div class="next-article clearfix">
@@ -6,8 +6,8 @@
 					<img class="next-article__img" src="" alt="">
 				</div>
 	    		<div class="next-article__content inline-block">
-	    			<span class="next-article__heading h5">Nästa artikel</span>
-	    			<h3 class="next-article__title"></h3>
+	    			<span class="next-article__heading rift-font h5">Nästa artikel</span>
+	    			<h3 class="next-article__title text-light"></h3>
 	    		</div>
 	    	</div>
 	    	<div class="loading-indicator">
