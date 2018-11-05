32aabab01e422efb0cd9c223df3add19ae52afe5
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-11-05 09:05:34 +0100

    Add grid-fixes and new logic for large small experiences

--- web/app/themes/visithalland/resources/views/taxonomy-experience.blade.php
+++ web/app/themes/visithalland/resources/views/taxonomy-experience.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     @include('partials.experience.experience-header')
-    @if(count($posts) > 4)
+    @if(count($featured_experiences) >= 5)
         @include('partials.experience.experience-large')
     @else
         @include('partials.experience.experience-small')
