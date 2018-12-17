69d25990e050001e76c287c00d39dbfa7b5b16cd
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-12-17 09:38:06 +0100

    Add truncation to next-article title

--- web/app/themes/visithalland/resources/views/partials/components/next-article.blade.php
+++ web/app/themes/visithalland/resources/views/partials/components/next-article.blade.php
@@ -5,7 +5,7 @@
 				<div class="overflow-hidden rounded relative h-24 w-24 -mt-8">
 					<img class="next-article__img absolute pin-l pin-t w-full h-auto" src="" alt="">
 				</div>
-	    		<div class="next-article__content pl-4 flex-1">
+	    		<div class="next-article__content pl-4 truncate whitespace-no-wrap flex-1">
 	    			<span class="next-article__heading block font-rift my-1 text-sm text-grey-light">@php _e( 'Nästa artikel', 'visithalland' ); @endphp</span>
 	    			<span class="next-article__title block font-rift font-bold text-white text-2xl max-w-8 whitespace-no-wrap truncate"></span>
 	    		</div>
