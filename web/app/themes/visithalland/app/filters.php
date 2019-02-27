80d7233505b2c6deb32c5940c16f7764437b12d7
sebastiansson <sebastian.marcusson@gmail.com>
2019-02-27 10:44:10 +0100

    Add acf recursive activity type

--- web/app/themes/visithalland/app/filters.php
+++ web/app/themes/visithalland/app/filters.php
@@ -15,6 +15,13 @@ add_filter('wp', function () {
     wp_redirect( '/happenings', 301 );
 }, 10, 0);
 
+// Add a day in halland as recursive acf fields
+add_filter( 'acf/rest_api/recursive/types', function( $types ) {
+    array_push($types, "activity")
+
+    return $types;
+});
+
 /**
  * Add <body> classes
  */
