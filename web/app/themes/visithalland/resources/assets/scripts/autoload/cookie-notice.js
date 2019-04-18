007aafdcd858f211489a22f47e2b3c79f3e91899
sebastiansson <sebastian.marcusson@gmail.com>
2019-04-18 10:00:00 +0200

    Fix cookie notice

--- web/app/themes/visithalland/resources/assets/scripts/autoload/cookie-notice.js
+++ web/app/themes/visithalland/resources/assets/scripts/autoload/cookie-notice.js
@@ -1,21 +1,21 @@
-import Cookies from 'js-cookie';
+import Cookies from 'js-cookie'
 
 class CookieNotice {
     constructor() {
-        this.bind();
+        $(document).ready(() => this.bind())
     }
 
     bind() {
         // Init Cookie-banner Star
         if (!Cookies.get('cookie_consent')) {
-            $(".cookie-banner").fadeIn(600);
+            $("#cookie-banner").fadeIn(600)            
         };
 
         $('#cookie-accept').on('click', function () {
-            Cookies.set('cookie_consent', 'comply', { expires: 5000 });
-            $(".cookie-banner").fadeOut(300); //$ to slide it up
+            Cookies.set('cookie_consent', 'comply', { expires: 5000 })
+            $("#cookie-banner").fadeOut(300) //$ to slide it up
         });
     }
 }
 
-export default new CookieNotice();
+export default new CookieNotice()
