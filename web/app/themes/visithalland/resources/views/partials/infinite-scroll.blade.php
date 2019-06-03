ac0f3d9636e88e5c27a0a4f40cf06179c4164fd5
David Öhlin <hello@davidohl.in>
2019-06-03 12:07:15 +0200

    update look on finished infinite scroll

--- web/app/themes/visithalland/resources/views/partials/infinite-scroll.blade.php
+++ web/app/themes/visithalland/resources/views/partials/infinite-scroll.blade.php
@@ -7,15 +7,13 @@
     <div id="nextPages" data-all='{{ json_encode($nextPostLinks) }}'></div>
 </div>
 
-<div class="page-load-status topographic-pattern">
-    <div class="container w-11/12  md:w-10/12  lg:w-10/12  mx-auto">
+<div class="page-load-status topographic-pattern bg-blue-light border-b border-px border-blue text-center">
+    <div class="container w-11/12 md:w-10/12 lg:w-10/12 mx-auto">
         <div class="infinite-scroll-last">
-            <span class="block mb-2">😐</span>
-            <span class="block">Vi hittar inte fler artiklar</span>
+            <span class="block font-rift font-bold text-white text-2xl py-4">😐 Vi hittar inte fler artiklar</span>
         </div>
         <div class="infinite-scroll-error">
-            <span class="block mb-2">😖</span>
-            <span class="block">Något gick fel</span>
+            <span class="block font-rift font-bold text-white text-2xl py-4">😖 Något gick fel! Vi kan inte hämta fler artiklar.</span>
         </div>
 
     </div>
