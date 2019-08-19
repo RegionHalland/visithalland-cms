b1fec9c75a8af35d4774b2fe8c33cfb7222f8a0a
sebastiansson <sebastian.marcusson@gmail.com>
2019-08-19 11:46:29 +0200

    Add support for campaign on front page

--- web/app/themes/visithalland/resources/views/partials/collections/experience-collection.blade.php
+++ web/app/themes/visithalland/resources/views/partials/collections/experience-collection.blade.php
@@ -1,44 +1,84 @@
-@if(is_array($navigation_items))
-    @foreach($navigation_items as $key => $navigation_item)
-        @if($loop->iteration === 1)
-            <article class="scrim overflow-hidden aspect-container aspect-5x4 sm:aspect-1 lg:aspect-16x9 relative mt-5 md:mt0 {{ $navigation_item->meta_fields['class_name'] }}">
-                @include(
-                    'partials.components.picture-element', 
+@if($campaign_mode)
+    <article class="scrim overflow-hidden aspect-container aspect-5x4 sm:aspect-1 lg:aspect-16x9 relative mt-5 md:mt0">
+        @include(
+            'partials.components.picture-element', 
+            [
+                'img_lg' => $campaign_img["sizes"]["vh_hero_wide"],
+                'img_lg_retina' => $campaign_img["sizes"]["vh_hero_wide@2x"],
+                'img_md' => $campaign_img["sizes"]["vh_medium_square"],
+                'img_md_retina' => $campaign_img["sizes"]["vh_medium_square@2x"],
+                'img_sm' => $campaign_img["sizes"]["vh_hero_tall"],
+                'img_sm_retina' => $campaign_img["sizes"]["vh_hero_tall@2x"],
+                'img' => $campaign_img["sizes"]["vh_hero_wide"],
+                'classes' => "absolute pin-l pin-t w-full", 
+                'img_alt' => $campaign_img["alt"]
+            ]
+        )
+        <div class="z-40 absolute container w-full sm:w-11/12 lg:w-10/12 flex justify-end flex-col pin-t mx-auto pin-b pin-r pin-l px-3 sm:px-0 pb-6 sm:pb-24">
+            <div>
+                @header(
                     [
-                        'img_lg' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_wide"],
-                        'img_lg_retina' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_wide@2x"],
-                        'img_md' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_medium_square"],
-                        'img_md_retina' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_medium_square@2x"],
-                        'img_sm' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_tall"],
-                        'img_sm_retina' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_tall@2x"],
-                        'img' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_wide"],
-                        'classes' => "absolute pin-l pin-t w-full", 
-                        'img_alt' => $navigation_item->meta_fields["cover_image"]["alt"]
+                        'title' => $campaign_title
                     ]
                 )
-                <div class="z-40 absolute container w-full sm:w-11/12 lg:w-10/12 flex justify-end flex-col pin-t mx-auto pin-b pin-r pin-l px-3 sm:px-0 pb-6 sm:pb-24">
-                    <div>
-                        @header(
-                            [
-                                'title' => "Populärt just nu"
-                            ]
-                        )
-                        @endheader
-                        <h1 class="text-white">{{ $navigation_item->post_title ? $navigation_item->post_title : $navigation_item->title }}</h1>
-                        @button(
-                            [
-                                'title' => $navigation_item->post_title ? $navigation_item->post_title : $navigation_item->title, 
-                                'url' => $navigation_item->url, 
-                                'icon' => "arrow-right-icon"
-                            ]
-                        )
-                        @endbutton
+                @endheader
+                <h1 class="text-white">{{ $campaign_excerpt }}</h1>
+                @button(
+                    [
+                        'title' => "Läs mer om " . $campaign_title, 
+                        'url' => $campaign_internal_link ? $campaign_internal_link : $campaign_external_link,
+                        'icon' => "arrow-right-icon"
+                    ]
+                )
+                @endbutton
+            </div>
+        </div>
+    </article>
+@else 
+    @if(is_array($navigation_items))
+        @foreach($navigation_items as $key => $navigation_item)
+            @if($loop->iteration === 1)
+                <article class="scrim overflow-hidden aspect-container aspect-5x4 sm:aspect-1 lg:aspect-16x9 relative mt-5 md:mt0 {{ $navigation_item->meta_fields['class_name'] }}">
+                    @include(
+                        'partials.components.picture-element', 
+                        [
+                            'img_lg' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_wide"],
+                            'img_lg_retina' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_wide@2x"],
+                            'img_md' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_medium_square"],
+                            'img_md_retina' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_medium_square@2x"],
+                            'img_sm' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_tall"],
+                            'img_sm_retina' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_tall@2x"],
+                            'img' => $navigation_item->meta_fields["cover_image"]["sizes"]["vh_hero_wide"],
+                            'classes' => "absolute pin-l pin-t w-full", 
+                            'img_alt' => $navigation_item->meta_fields["cover_image"]["alt"]
+                        ]
+                    )
+                    <div class="z-40 absolute container w-full sm:w-11/12 lg:w-10/12 flex justify-end flex-col pin-t mx-auto pin-b pin-r pin-l px-3 sm:px-0 pb-6 sm:pb-24">
+                        <div>
+                            @header(
+                                [
+                                    'title' => "Populärt just nu"
+                                ]
+                            )
+                            @endheader
+                            <h1 class="text-white">{{ $navigation_item->post_title ? $navigation_item->post_title : $navigation_item->title }}</h1>
+                            @button(
+                                [
+                                    'title' => $navigation_item->post_title ? $navigation_item->post_title : $navigation_item->title, 
+                                    'url' => $navigation_item->url, 
+                                    'icon' => "arrow-right-icon"
+                                ]
+                            )
+                            @endbutton
+                        </div>
                     </div>
-                </div>
-            </article>
-        @endif
-    @endforeach
+                </article>
+            @endif
+        @endforeach
+    @endif
 @endif
+
+
 <div class="container w-11/12 lg:w-10/12 mx-auto mt-4 sm:-mt-16 relative mb-8">
     @if(is_array($navigation_items))
         @header(
